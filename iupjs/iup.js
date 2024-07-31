96748e562d745143fecd405fff9d39d2d9ec959c
fcanudo <joao678@gmail.com>
2024-07-31 10:53:21 -0300

    better windows/linux lib handling

--- iupjs/iup.js
+++ iupjs/iup.js
@@ -46,15 +46,19 @@ import { fontdlg } from "./dialogs/fontdlg";
 import { progressdlg } from "./dialogs/progressdlg";
 import { scintilladlg } from "./dialogs/scintilladlg";
 
-import { iup_lib, iupfiledlg_lib, iupcontrols_lib, iupim_lib, iup_scintilla_lib, iupimglib_lib } from "./utils/import_libs";
-import { iup_definitions } from "./iup_lib/definitions";
+import {
+    iup_lib_win, iupfiledlg_lib_win, iupcontrols_lib_win, iupim_lib_win, iup_scintilla_lib_win, iupimglib_lib_win,
+    iup_lib_linux, iupcontrols_lib_linux, iupim_lib_linux, iup_scintilla_lib_linux, iupimglib_lib_linux
+} from "./utils/import_libs";
 
+import { iup_definitions } from "./iup_lib/definitions";
 const { ptr, cstring, i32 } = FFIType;
 
-let path = iup_lib;
+let path = process.platform === 'win32' ? iup_lib_win : iup_lib_linux;
 
 export const {
     symbols: {
+        // Functions
         IupOpen,
         IupMessage,
         IupMessageError,
@@ -77,7 +81,7 @@ export const {
         IupLoopStep,
         IupSetFunction,
         IupPopup,
-
+        // Controls
         IupButton,
         IupText,
         IupLabel,
@@ -111,7 +115,7 @@ export const {
         IupItem,
         IupSubmenu,
         IupSeparator,
-
+        // Dialogs
         IupFileDlg,
         IupMessageDlg,
         IupColorDlg,
@@ -120,7 +124,7 @@ export const {
     }
 } = dlopen(path, iup_definitions);
 
-const pathIupControls = iupcontrols_lib;
+const pathIupControls = process.platform === 'win32' ? iupcontrols_lib_win : iupcontrols_lib_linux;
 
 export const {
     symbols: {
@@ -138,7 +142,7 @@ export const {
     IupMatrixList: { args: [], returns: ptr },
 });
 
-const pathIupScintilla = iup_scintilla_lib;
+const pathIupScintilla = process.platform === 'win32' ? iup_scintilla_lib_win : iup_scintilla_lib_linux;
 
 export const {
     symbols: {
@@ -148,11 +152,11 @@ export const {
     },
 } = dlopen(pathIupScintilla, {
     IupScintillaOpen: { args: [], returns: i32 },
-    IupScintilla:  { args: [], returns: ptr }, 
+    IupScintilla: { args: [], returns: ptr },
     IupScintillaDlg: { args: [], returns: ptr },
 });
 
-const pathIupImgLib = iupimglib_lib;
+const pathIupImgLib = process.platform === 'win32' ? iupimglib_lib_win : iupimglib_lib_linux;
 
 export const {
     symbols: {
@@ -162,7 +166,7 @@ export const {
     IupImageLibOpen: { args: [], returns: i32 },
 });
 
-const pathIupIm = iupim_lib;
+const pathIupIm = process.platform === 'win32' ? iupim_lib_win : iupim_lib_linux;
 
 export const {
     symbols: {
@@ -172,15 +176,22 @@ export const {
     IupLoadImage: { args: [cstring], returns: ptr }
 });
 
-const pathIupfiledlg = iupfiledlg_lib;
+const pathIupfiledlg = iupfiledlg_lib_win;
+let iupfiledlgLib = null;
+
+if (process.platform === 'win32') {
+    iupfiledlgLib = dlopen(pathIupfiledlg, {
+        IupNewFileDlgOpen: { args: [], returns: ptr }
+    });
+} else {
+    iupfiledlgLib = { symbols: { IupNewFileDlgOpen: () => { } } }
+}
 
 export const {
     symbols: {
         IupNewFileDlgOpen
     },
-} = dlopen(pathIupfiledlg, {
-    IupNewFileDlgOpen: { args: [], returns: ptr }
-});
+} = iupfiledlgLib;
 
 export function str(template, ...values) {
     return Buffer.from(`${template.map((component, index) => values[index] ? component + values[index] : component).join('')}\0`);
@@ -199,7 +210,7 @@ export class iup {
         IupOpen();
         IupSetGlobal(str`UTF8MODE`, str`YES`);
         IupSetGlobal(str`UTF8MODE_FILE`, str`YES`);
-        if (process.platform === 'win32') IupNewFileDlgOpen();
+        IupNewFileDlgOpen();
     }
 
     static mainLoop() {
@@ -246,24 +257,108 @@ export class iup {
     }
 }
 
-const IUP_CENTER = 0xFFFF;  /* 65535 */
-const IUP_LEFT = 0xFFFE;  /* 65534 */
-const IUP_RIGHT = 0xFFFD;  /* 65533 */
-const IUP_MOUSEPOS = 0xFFFC;  /* 65532 */
-const IUP_CURRENT = 0xFFFB;  /* 65531 */
-const IUP_CENTERPARENT = 0xFFFA;  /* 65530 */
+const IUP_ERROR = 1;
+const IUP_NOERROR = 0;
+const IUP_OPENED = -1;
+const IUP_INVALID = -1;
+const IUP_INVALID_ID = -10;
+const IUP_IGNORE = -1;
+const IUP_DEFAULT = -2;
+const IUP_CLOSE = -3;
+const IUP_CONTINUE = -4;
+const IUP_CENTER = 0xFFFF;
+const IUP_LEFT = 0xFFFE;
+const IUP_RIGHT = 0xFFFD;
+const IUP_MOUSEPOS = 0xFFFC;
+const IUP_CURRENT = 0xFFFB;
+const IUP_CENTERPARENT = 0xFFFA;
+const IUP_LEFTPARENT = 0xFFF9;
+const IUP_RIGHTPARENT = 0xFFF8;
 const IUP_TOP = IUP_LEFT;
 const IUP_BOTTOM = IUP_RIGHT;
+const IUP_TOPPARENT = IUP_LEFTPARENT;
+const IUP_BOTTOMPARENT = IUP_RIGHTPARENT;
+const IUP_BUTTON1 = '1';
+const IUP_BUTTON2 = '2';
+const IUP_BUTTON3 = '3';
+const IUP_BUTTON4 = '4';
+const IUP_BUTTON5 = '5';
+const IUP_MASK_FLOAT = "[+/-]?(/d+/.?/d*|/./d+)";
+const IUP_MASK_UFLOAT = "(/d+/.?/d*|/./d+)";
+const IUP_MASK_EFLOAT = "[+/-]?(/d+/.?/d*|/./d+)([eE][+/-]?/d+)?";
+const IUP_MASK_UEFLOAT = "(/d+/.?/d*|/./d+)([eE][+/-]?/d+)?";
+const IUP_MASK_FLOATCOMMA = "[+/-]?(/d+/,?/d*|/,/d+)";
+const IUP_MASK_UFLOATCOMMA = "(/d+/,?/d*|/,/d+)";
+const IUP_MASK_INT = "[+/-]?/d+";
+const IUP_MASK_UINT = "/d+";
+const IUPMASK_FLOAT = IUP_MASK_FLOAT;
+const IUPMASK_UFLOAT = IUP_MASK_UFLOAT;
+const IUPMASK_EFLOAT = IUP_MASK_EFLOAT;
+const IUPMASK_INT = IUP_MASK_INT;
+const IUPMASK_UINT = IUP_MASK_UINT;
+const IUP_GETPARAM_BUTTON1 = -1;
+const IUP_GETPARAM_INIT = -2;
+const IUP_GETPARAM_BUTTON2 = -3;
+const IUP_GETPARAM_BUTTON3 = -4;
+const IUP_GETPARAM_CLOSE = -5;
+const IUP_GETPARAM_MAP = -6;
+const IUP_GETPARAM_OK = IUP_GETPARAM_BUTTON1;
+const IUP_GETPARAM_CANCEL = IUP_GETPARAM_BUTTON2;
+const IUP_GETPARAM_HELP = IUP_GETPARAM_BUTTON3;
+const IUP_PRIMARY = -1;
+const IUP_SECONDARY = -2;
 
 export {
+    IUP_ERROR,
+    IUP_NOERROR,
+    IUP_OPENED,
+    IUP_INVALID,
+    IUP_INVALID_ID,
+    IUP_IGNORE,
+    IUP_DEFAULT,
+    IUP_CLOSE,
+    IUP_CONTINUE,
     IUP_CENTER,
     IUP_LEFT,
     IUP_RIGHT,
     IUP_MOUSEPOS,
     IUP_CURRENT,
     IUP_CENTERPARENT,
+    IUP_LEFTPARENT,
+    IUP_RIGHTPARENT,
     IUP_TOP,
     IUP_BOTTOM,
+    IUP_TOPPARENT,
+    IUP_BOTTOMPARENT,
+    IUP_BUTTON1,
+    IUP_BUTTON2,
+    IUP_BUTTON3,
+    IUP_BUTTON4,
+    IUP_BUTTON5,
+    IUP_MASK_FLOAT,
+    IUP_MASK_UFLOAT,
+    IUP_MASK_EFLOAT,
+    IUP_MASK_UEFLOAT,
+    IUP_MASK_FLOATCOMMA,
+    IUP_MASK_UFLOATCOMMA,
+    IUP_MASK_INT,
+    IUP_MASK_UINT,
+    IUPMASK_FLOAT,
+    IUPMASK_UFLOAT,
+    IUPMASK_EFLOAT,
+    IUPMASK_INT,
+    IUPMASK_UINT,
+    IUP_GETPARAM_BUTTON1,
+    IUP_GETPARAM_INIT,
+    IUP_GETPARAM_BUTTON2,
+    IUP_GETPARAM_BUTTON3,
+    IUP_GETPARAM_CLOSE,
+    IUP_GETPARAM_MAP,
+    IUP_GETPARAM_OK,
+    IUP_GETPARAM_CANCEL,
+    IUP_GETPARAM_HELP,
+    IUP_PRIMARY,
+    IUP_SECONDARY,
 
     backgroundbox,
     button,
